537480da1c60837a4d259972815f3d3146f8d837
Magdalena Bień <bien@magda.krakow.pl>
2024-01-22 22:07:17 +0100

    correcting wrong table numer

--- src/components/features/EditTableForm/EditTableForm.js
+++ src/components/features/EditTableForm/EditTableForm.js
@@ -1,32 +1,32 @@
-import { useDispatch } from 'react-redux';
-import { editTableRequest } from '../../../redux/tablesRedux';
-import { useNavigate } from 'react-router-dom';
-import TableForm from '../TableForm/TableForm';
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router';
-import { getTableById } from '../../../redux/tablesRedux';
-import { Navigate } from 'react-router-dom';
-
+import { useDispatch } from "react-redux";
+import { editTableRequest } from "../../../redux/tablesRedux";
+import { useNavigate } from "react-router-dom";
+import TableForm from "../TableForm/TableForm";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router";
+import { getTableById } from "../../../redux/tablesRedux";
+import { Navigate } from "react-router-dom";
 
 const EditTableForm = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const { id } = useParams();
-    const form = useSelector(state => getTableById(state, id));    
-    if(!form) return <Navigate to="/" />;
+  const { id } = useParams();
+  const form = useSelector((state) => getTableById(state, id));
+  if (form === undefined) return <Navigate to="/" />;
 
+  const handleSubmit = (form) => {
+    dispatch(editTableRequest(form));
+    navigate("/");
+  };
 
-    const handleSubmit = form => {
-        dispatch(editTableRequest(form));     
-        navigate("/");
-    }
-
-	return (
-        <TableForm actionHandle={handleSubmit} buttonName="UPDATE" formState={form}></TableForm>
-	);
-
+  return (
+    <TableForm
+      actionHandle={handleSubmit}
+      buttonName="UPDATE"
+      formState={form}
+    ></TableForm>
+  );
 };
 
 export default EditTableForm;
-
--- src/components/pages/Table/Table.js
+++ src/components/pages/Table/Table.js
@@ -1,37 +1,31 @@
 import { useParams } from "react-router";
-import { useDispatch } from "react-redux";
 import { Navigate } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { getTableById } from "../../../redux/tablesRedux";
-import { getAllStatuts } from "../../../redux/statutsRedux";
 import { isLoadingStatuts } from "../../../redux/statutsRedux";
+import { getStatusById } from "../../../redux/statutsRedux";
 
 const Table = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const statutsList = useSelector(getAllStatuts);
+  //const navigate = useNavigate();
 
   const { id } = useParams();
   const table = useSelector((state) => getTableById(state, id));
-
-  const findStatus = (statusId) =>
-    statutsList.find((status) => status.id === statusId).statusName;
+  const status = useSelector((state) => getStatusById(state, id));
 
   const isLoadingDataStatuts = useSelector(isLoadingStatuts);
   if (isLoadingDataStatuts) {
     return <p>Loading ...</p>;
   }
 
-  if (!table) return <Navigate to="/" />;
+  if (table === undefined) return <Navigate to="/" />;
 
   return (
     <div>
       <h1>Table</h1>
       <p>
         Chosen table number: {id} <br />
-        Status: {findStatus(table.statusId)}
+        Status: {status.statusName}
         <br />
         People: {table.peopleAmount} / {table.maxPeopleAmount} <br />
         Bill: $ {table.bill}
